9de92d09a54944b7cf63c98fb5359f841850fe83
Andrii Skrypnychenko <scrip.a.v@gmail.com>
2018-04-01 23:47:28 +0200

    Docker Compose for Travis

--- src/test/java/flow/DeclinedPaymentFlow.java
+++ src/test/java/flow/DeclinedPaymentFlow.java
@@ -30,7 +30,7 @@ public class DeclinedPaymentFlow {
         Configuration.collectionsTimeout = 8000;
         Configuration.baseUrl = "https://www.ryanair.com/ie/en";
         Configuration.reportsFolder = "target/surefire-reports";
-        Configuration.headless=true;
+//        Configuration.headless=true;
         open("/");
     }
 
